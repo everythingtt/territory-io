9bf45ec3a6884bf61cf1eee9b9a911930edca879
territory.io <forbusiness68.8.65.43@gmail.com>
2026-03-19 14:33:13 +0200

    Standalone ToryEngine Studio: Remove Tailwind CDN dependency

---
+++ APK-ready-ToryEngine/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'toryengine-studio-v1.0.0';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'manifest.webmanifest',
+  'assets/rocket.gif'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => console.log("SW Install Cache error", err));
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
